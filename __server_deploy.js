8410e9ccd87d852039fa1d01257fee53d57de193
Amir Hossain <amir508_du@yahoo.com>
2022-04-28 04:17:25 +0600

    set api to get user wise orders

---
+++ __server_deploy.js
@@ -0,0 +1,26 @@
+/* One time for PC 
+* 1. create heroku account
+* 2. verify email
+* 3. install heroku cli
+* 4. heroku login
+*-------------------------
+* For each project one time
+* 1. heroku create
+* 2. make sure you: git add . git commit. git push
+* 3. git push heroku main
+* 4. go to heroku dashboard > current project > settings > Reveal config vars
+* 5. copy paste config vars from your .env file
+* 6. make sure you have whitelisted all ip address to access mongodb
+* --------------------------------------------------
+* update server
+* ---------------
+* 1. make changes
+* 2. make sure you: git add.git commit.git push 
+* 3. git push heroku main
+* ------------------------------------------------
+* connect server with client and deploy client
+* --------------------------------
+* 1. replace local host by heroku link
+* 2. npm run build
+* 3. firebase deploy
+*/
--- index.js
+++ index.js
@@ -64,7 +64,8 @@ async function run () {
 
     //load orders api
     app.get('/orders', async(req, res) => {
-        const query = {};
+        const email = req.query.email;
+        const query = {email:email};
         const cursor = orderCollection.find(query);
         const orders = await cursor.toArray();
         res.send(orders);
